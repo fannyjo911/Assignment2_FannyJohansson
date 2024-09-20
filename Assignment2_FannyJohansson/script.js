869f0ee83eea5a5560551134672b992b7511bcf9
fannyjo911 <fanny.f.johansson@gmail.com>
2024-09-20 12:59:40 +0200

    Add files via upload

---
+++ Assignment2_FannyJohansson/script.js
@@ -0,0 +1,26 @@
+
+let age = 33
+let vip = true
+ 
+if(vip == true){
+    console.log('Acess granted')
+}
+
+else{
+    if(age >= 18) {
+        console.log('Welcome!')
+    }else{
+        console.log('Access denied')
+    }
+
+}
+    
+   
+
+       
+
+    
+    
+
+
+
